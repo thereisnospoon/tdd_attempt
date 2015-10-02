823298fba32bde0e8e3fd32142d67c7732b67eee
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 10:55:19 +0300

    Test for first token of message validation

--- src/test/java/com/epam/tdd/InstructionMessageParserTest.java
+++ src/test/java/com/epam/tdd/InstructionMessageParserTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertEquals;
 public class InstructionMessageParserTest {
 
 	private static final String INVALID_INSTRUCTION_MESSAGE = "InstructionMessage A MZ89 5678 50";
+	private static final String INSTRUCTION_MESSAGE_WITH_INVALID_FIRST_TOKEN =
+			"InstructionMessage A MZ89 5678 50 2015-03-05T10:04:56.012Z";
 
 	private static final String VALID_INSTRUCTION_MESSAGE = "InstructionMessage A MZ89 5678 50 2015-03-05T10:04:56.012Z";
 	private static final String INSTRUCTION_TYPE_FROM_VALID_MESSAGE = "A";
@@ -23,7 +25,12 @@ public class InstructionMessageParserTest {
 	}
 
 	@Test(expected = InstructionMessageParsingException.class)
-	public void shouldThrownExceptionWhenInvalidNumberOfTokens() {
+	public void shouldThrowExceptionWhenInvalidNumberOfTokens() {
 		testedInstance.parse(INVALID_INSTRUCTION_MESSAGE);
+	}
+
+	@Test(expected = InstructionMessageParsingException.class)
+	public void shouldThrowExceptionWhenIncorrectFirstToken() {
+		testedInstance.parse(INSTRUCTION_MESSAGE_WITH_INVALID_FIRST_TOKEN);
 	}
 }

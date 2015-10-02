c9561b1e80ada892bb21f6b2311d3ccbf19b7fa4
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 15:31:06 +0300

    Test for received message enqueue

--- src/test/java/com/epam/tdd/MessageReceiverImplTest.java
+++ src/test/java/com/epam/tdd/MessageReceiverImplTest.java
@@ -9,11 +9,13 @@ import org.mockito.runners.MockitoJUnitRunner;
 import com.epam.tdd.parsing.InstructionMessageParser;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MessageReceiverImplTest {
 
-	private static final String INSTRUCTION_MESSAGE = "InstructionMessage A MZ89 5678 50 2015-03-05T10:04:56.012Z";
+	private static final String INSTRUCTION_MESSAGE_STRING = "InstructionMessage A MZ89 5678 50 2015-03-05T10:04:56.012Z";
+	private static final InstructionMessage INSTRUCTION_MESSAGE = new InstructionMessage();
 
 	@InjectMocks
 	private MessageReceiverImpl testedInstance = new MessageReceiverImpl();
@@ -21,10 +23,22 @@ public class MessageReceiverImplTest {
 	@Mock
 	private InstructionMessageParser instructionMessageParser;
 
+	@Mock
+	private InstructionQueue instructionQueue;
+
 	@Test
 	public void shouldParseReceivedMessage() {
 
-		testedInstance.receive(INSTRUCTION_MESSAGE);
-		verify(instructionMessageParser).parse(INSTRUCTION_MESSAGE);
+		testedInstance.receive(INSTRUCTION_MESSAGE_STRING);
+		verify(instructionMessageParser).parse(INSTRUCTION_MESSAGE_STRING);
+	}
+
+	@Test
+	public void shouldAddReceivedMessageToQueue() {
+
+		when(instructionMessageParser.parse(INSTRUCTION_MESSAGE_STRING)).thenReturn(INSTRUCTION_MESSAGE);
+
+		testedInstance.receive(INSTRUCTION_MESSAGE_STRING);
+		verify(instructionQueue).enqueue(INSTRUCTION_MESSAGE);
 	}
 }

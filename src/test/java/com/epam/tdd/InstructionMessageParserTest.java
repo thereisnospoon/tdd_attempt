e07180a8dbfffefaec2229c6b1934e15a0c711ab
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 10:49:38 +0300

    Test for instruction type parsing

--- src/test/java/com/epam/tdd/InstructionMessageParserTest.java
+++ src/test/java/com/epam/tdd/InstructionMessageParserTest.java
@@ -4,11 +4,23 @@ import com.epam.tdd.parsing.InstructionMessageParser;
 import com.epam.tdd.parsing.InstructionMessageParsingException;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class InstructionMessageParserTest {
 
 	private static final String INVALID_INSTRUCTION_MESSAGE = "InstructionMessage A MZ89 5678 50";
 
+	private static final String VALID_INSTRUCTION_MESSAGE = "InstructionMessage A MZ89 5678 50 2015-03-05T10:04:56.012Z";
+	private static final String INSTRUCTION_TYPE_FROM_VALID_MESSAGE = "A";
+
 	private InstructionMessageParser testedInstance = new InstructionMessageParser();
+
+	@Test
+	public void shouldSetParsedInstructionType() {
+
+		InstructionMessage instructionMessage = testedInstance.parse(VALID_INSTRUCTION_MESSAGE);
+		assertEquals(INSTRUCTION_TYPE_FROM_VALID_MESSAGE, instructionMessage.getInstructionType());
+	}
 
 	@Test(expected = InstructionMessageParsingException.class)
 	public void shouldThrownExceptionWhenInvalidNumberOfTokens() {

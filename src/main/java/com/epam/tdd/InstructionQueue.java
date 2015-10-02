f27204138efc38d5c0a4274df55d58ec6a59decd
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 15:17:47 +0300

    Implemented queue to support FIFO in case of equal priorities

--- src/main/java/com/epam/tdd/InstructionQueue.java
+++ src/main/java/com/epam/tdd/InstructionQueue.java
@@ -8,14 +8,45 @@ import java.util.Queue;
 
 public class InstructionQueue {
 
-	private Queue<InstructionMessage> messageQueue;
+	private static class InstructionMessageWrapper {
+
+		private InstructionMessage instructionMessage;
+		private long serialNumber;
+
+		public InstructionMessageWrapper(InstructionMessage instructionMessage, long serialNumber) {
+			this.instructionMessage = instructionMessage;
+			this.serialNumber = serialNumber;
+		}
+
+		public InstructionMessage getInstructionMessage() {
+			return instructionMessage;
+		}
+
+		public long getSerialNumber() {
+			return serialNumber;
+		}
+	}
+
+	private static final Comparator<InstructionMessageWrapper> BY_SERIAL_NUMBER_COMPARATOR =
+			Comparator.comparing(InstructionMessageWrapper::getSerialNumber);
+
+	private static final Comparator<InstructionMessageWrapper> BY_PRIORITY_COMPARATOR =
+			Comparator.comparing(wrapper -> wrapper.getInstructionMessage().getInstructionType().getPriority());
+
+	private static final Comparator<InstructionMessageWrapper> MESSAGES_COMPARATOR = BY_PRIORITY_COMPARATOR
+			.thenComparing(BY_SERIAL_NUMBER_COMPARATOR);
+
+	private Queue<InstructionMessageWrapper> messageQueue;
 
 	private InstructionMessageValidator instructionMessageValidator;
 
+	private long messagesCounter;
+
 	public InstructionQueue() {
 
-		messageQueue = new PriorityQueue<>(Comparator.comparing(message -> message.getInstructionType().getPriority()));
+		messageQueue = new PriorityQueue<>(MESSAGES_COMPARATOR);
 		instructionMessageValidator = new InstructionMessageValidator();
+		messagesCounter = 0;
 	}
 
 	public boolean isEmpty() {
@@ -25,11 +56,11 @@ public class InstructionQueue {
 	public void enqueue(InstructionMessage instructionMessage) {
 
 		instructionMessageValidator.validate(instructionMessage);
-		messageQueue.add(instructionMessage);
+		messageQueue.add(new InstructionMessageWrapper(instructionMessage, ++messagesCounter));
 	}
 
 	public InstructionMessage dequeue() {
-		return messageQueue.poll();
+		return messageQueue.poll().getInstructionMessage();
 	}
 
 	public int count() {
@@ -37,6 +68,6 @@ public class InstructionQueue {
 	}
 
 	public InstructionMessage peek() {
-		return messageQueue.peek();
+		return messageQueue.peek().getInstructionMessage();
 	}
 }

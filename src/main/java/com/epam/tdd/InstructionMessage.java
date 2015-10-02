79ed8065e242886a997fe9308cc93b3c8e9034ec
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 10:52:30 +0300

    Implemented parsing of instruction type

---
+++ src/main/java/com/epam/tdd/InstructionMessage.java
@@ -0,0 +1,14 @@
+package com.epam.tdd;
+
+public class InstructionMessage {
+
+	private String instructionType;
+
+	public String getInstructionType() {
+		return instructionType;
+	}
+
+	public void setInstructionType(String instructionType) {
+		this.instructionType = instructionType;
+	}
+}
--- src/main/java/com/epam/tdd/parsing/InstructionMessageParser.java
+++ src/main/java/com/epam/tdd/parsing/InstructionMessageParser.java
@@ -1,15 +1,23 @@
 package com.epam.tdd.parsing;
 
+import com.epam.tdd.InstructionMessage;
+
 public class InstructionMessageParser {
 
-	private static final int NUMBER_OF_TOKENS_IN_MESSAGE = 6;
 	private static final String MESSAGE_DELIMITER = "\\s";
+	private static final int NUMBER_OF_TOKENS_IN_MESSAGE = 6;
+	private static final int POSITION_OF_INSTRUCTION_TYPE = 1;
 
-	public void parse(String instructionMessageString) {
+	public InstructionMessage parse(String instructionMessageString) {
 
 		String[] messageTokens = instructionMessageString.split(MESSAGE_DELIMITER);
 		if (NUMBER_OF_TOKENS_IN_MESSAGE != messageTokens.length) {
 			throw new InstructionMessageParsingException("Invalid number of tokens in message");
 		}
+
+		InstructionMessage instructionMessage = new InstructionMessage();
+		instructionMessage.setInstructionType(messageTokens[POSITION_OF_INSTRUCTION_TYPE]);
+
+		return instructionMessage;
 	}
 }

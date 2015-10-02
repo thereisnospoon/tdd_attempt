43454c12b984553577c6cc8d8564fd9743eae699
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 11:11:19 +0300

    Tests for message integer parameters parsing

--- src/test/java/com/epam/tdd/InstructionMessageParserTest.java
+++ src/test/java/com/epam/tdd/InstructionMessageParserTest.java
@@ -15,6 +15,8 @@ public class InstructionMessageParserTest {
 	private static final String VALID_INSTRUCTION_MESSAGE = "InstructionMessage A MZ89 5678 50 2015-03-05T10:04:56.012Z";
 	private static final String INSTRUCTION_TYPE_FROM_VALID_MESSAGE = "A";
 	private static final String PRODUCT_CODE_FROM_VALID_MESSAGE = "MZ89";
+	private static final int QUANTITY_FROM_VALID_MESSAGE = 5678;
+	private static final int UOM_FROM_VALID_MESSAGE = 50;
 
 	private InstructionMessageParser testedInstance = new InstructionMessageParser();
 
@@ -30,6 +32,20 @@ public class InstructionMessageParserTest {
 
 		InstructionMessage instructionMessage = testedInstance.parse(VALID_INSTRUCTION_MESSAGE);
 		assertEquals(PRODUCT_CODE_FROM_VALID_MESSAGE, instructionMessage.getProductCode());
+	}
+
+	@Test
+	public void shouldSetParsedQuantity() {
+
+		InstructionMessage instructionMessage = testedInstance.parse(VALID_INSTRUCTION_MESSAGE);
+		assertEquals(QUANTITY_FROM_VALID_MESSAGE, instructionMessage.getQuantity());
+	}
+
+	@Test
+	public void shouldSetParsedUom() {
+
+		InstructionMessage instructionMessage = testedInstance.parse(VALID_INSTRUCTION_MESSAGE);
+		assertEquals(UOM_FROM_VALID_MESSAGE, instructionMessage.getUom());
 	}
 
 	@Test(expected = InstructionMessageParsingException.class)

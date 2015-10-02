0e68a04eb010340fedb7ff016d116819ee00e5c0
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 10:59:54 +0300

    Implemented validation of first token in message

--- src/main/java/com/epam/tdd/parsing/InstructionMessageParser.java
+++ src/main/java/com/epam/tdd/parsing/InstructionMessageParser.java
@@ -5,7 +5,10 @@ import com.epam.tdd.InstructionMessage;
 public class InstructionMessageParser {
 
 	private static final String MESSAGE_DELIMITER = "\\s";
+	private static final String FIRST_TOKEN_OF_MESSAGE = "InstructionMessage";
+
 	private static final int NUMBER_OF_TOKENS_IN_MESSAGE = 6;
+	private static final int POSITION_OF_FIRST_TOKEN = 0;
 	private static final int POSITION_OF_INSTRUCTION_TYPE = 1;
 
 	public InstructionMessage parse(String instructionMessageString) {
@@ -13,6 +16,11 @@ public class InstructionMessageParser {
 		String[] messageTokens = instructionMessageString.split(MESSAGE_DELIMITER);
 		if (NUMBER_OF_TOKENS_IN_MESSAGE != messageTokens.length) {
 			throw new InstructionMessageParsingException("Invalid number of tokens in message");
+		}
+
+		if (!FIRST_TOKEN_OF_MESSAGE.equals(messageTokens[POSITION_OF_FIRST_TOKEN])) {
+			throw new InstructionMessageParsingException("First token of message is invalid: "
+					+ messageTokens[POSITION_OF_FIRST_TOKEN]);
 		}
 
 		InstructionMessage instructionMessage = new InstructionMessage();

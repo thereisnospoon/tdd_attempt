4892cef7c23089ebd866c6d0def52115e29cf121
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 13:10:07 +0300

    Test for check of queue emptiness after message enqueue

--- src/test/java/com/epam/tdd/InstructionQueueTest.java
+++ src/test/java/com/epam/tdd/InstructionQueueTest.java
@@ -3,6 +3,7 @@ package com.epam.tdd;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class InstructionQueueTest {
@@ -19,5 +20,13 @@ public class InstructionQueueTest {
 	@Test
 	public void shouldBeEmptyQueue() {
 		assertTrue(testedInstance.isEmpty());
+	}
+
+	@Test
+	public void shouldBeNonEmpty() {
+
+		assertTrue(testedInstance.isEmpty());
+		testedInstance.enqueue(instructionMessage);
+		assertFalse(testedInstance.isEmpty());
 	}
 }

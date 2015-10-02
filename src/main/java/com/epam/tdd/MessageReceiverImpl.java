d4d7b6f85ba951e0bf273f88011245c72377bfca
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-10-02 15:32:04 +0300

    Implemented enqueue of received message

--- src/main/java/com/epam/tdd/MessageReceiverImpl.java
+++ src/main/java/com/epam/tdd/MessageReceiverImpl.java
@@ -6,14 +6,18 @@ public class MessageReceiverImpl implements MessageReceiver {
 
 	private InstructionMessageParser instructionMessageParser;
 
+	private InstructionQueue instructionQueue;
+
 	public MessageReceiverImpl() {
+
 		instructionMessageParser = new InstructionMessageParser();
+		instructionQueue = new InstructionQueue();
 	}
 
 	@Override
 	public void receive(String message) {
 
 		InstructionMessage instructionMessage = instructionMessageParser.parse(message);
-
+		instructionQueue.enqueue(instructionMessage);
 	}
 }
